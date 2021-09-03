bb05a363e3d02beab104fba06aa6e600d0ae4798
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 <irene.anran.niu@gmail.com>
2021-09-03 12:23:53 +1200

    Add original implementation

--- strategy/my/cache.go
+++ strategy/my/cache.go
@@ -1,14 +1,23 @@
 package main
 
+import "time"
+
 type cache struct {
-	storage      map[string]string
+	storage      map[string]*item
 	evictionAlgo evictionAlgo
 	capacity     int
 	maxCapacity  int
 }
 
+type item struct {
+	value        string
+	lastUpdated  time.Time
+	lastAccessed time.Time
+	accessCount  int
+}
+
 func initCache(e evictionAlgo) *cache {
-	storage := make(map[string]string)
+	storage := make(map[string]*item)
 	return &cache{
 		storage:      storage,
 		evictionAlgo: e,
@@ -26,11 +35,20 @@ func (c *cache) add(key, value string) {
 		c.evict()
 	}
 	c.capacity++
-	c.storage[key] = value
+	c.storage[key] = &item{
+		value:       value,
+		lastUpdated: time.Now(),
+		accessCount: 0}
 }
 
-func (c *cache) get(key string) {
-	delete(c.storage, key)
+func (c *cache) get(key string) (string, bool) {
+	item, got := c.storage[key]
+	if !got {
+		return "", false
+	}
+	item.lastAccessed = time.Now()
+	item.accessCount++
+	return item.value, true
 }
 
 func (c *cache) evict() {
